0d23684d820370b513097d913c894a770f850ff5
Sasha <sashavanzuriak@gmail.com>
2020-05-05 11:09:54 +0300

    Create project

---
+++ src/header/header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./header.css";
+
+export function Header() {
+  return (
+    <div className="header">
+      <p className="headerText">Music Life</p>
+    </div>
+  );
+}
