f8b44daa54b45489eaab900eab3aaa2cddb8eed9
Sasha <sashavanzuriak@gmail.com>
2020-05-11 11:09:44 +0300

    add new class

--- src/body/body.js
+++ src/body/body.js
@@ -1,12 +1,29 @@
 import React from "react";
 import "./body.css";
 import daft from "./daft.jpg";
+import { PresetColorTypes } from "antd/lib/_util/colors";
+
+class Cycle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.setState({ name: "hello" });
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle = () => {
+    this.name = "DaftPunkImg";
+  };
+
+  render() {
+    return <img src={daft} className={this.name} onClick={this.toggle}></img>;
+  }
+}
 
 export function BodySite() {
   return (
     <div className="BodySite">
       <p className="DaftPunkText">Daft Punk</p>
-      <img src={daft} className="DaftPunkImg"></img>
+      <Cycle />
     </div>
   );
 }
