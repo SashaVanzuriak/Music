1526e98539faac86c6f15fbcb80da53d03bb8bb1
Sasha <sashavanzuriak@gmail.com>
2020-05-16 11:54:53 +0300

    add auto music

--- src/body/musicControler/musicControl.js
+++ src/body/musicControler/musicControl.js
@@ -2,18 +2,27 @@ import React from "react";
 import "./musicControl.css";
 import daft from "./daft.jpg";
 import { Icon } from "evergreen-ui";
+import GetLucky from "./GetLucky.mp3";
 
 export class Cycle extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { name: "NonClick", iconName: "play" };
+    this.state = { name: "NonClick", iconName: "play", audioPlay: "pause" };
     this.toggle = this.toggle.bind(this);
   }
 
   toggle = () => {
-    this.setState((state) => ({ name: "DaftPunkImg", iconName: "pause" }));
+    this.setState((state) => ({
+      name: "DaftPunkImg",
+      iconName: "pause",
+      audioPlay: "play",
+    }));
     if (this.state.name == "DaftPunkImg") {
-      this.setState((state) => ({ name: "NonClick", iconName: "play" }));
+      this.setState((state) => ({
+        name: "NonClick",
+        iconName: "play",
+        audioPlay: "",
+      }));
     }
   };
 
@@ -22,6 +31,11 @@ export class Cycle extends React.Component {
       <div>
         <img src={daft} className={this.state.name}></img>
         <Icon icon={this.state.iconName} size={80} onClick={this.toggle} />
+        <audio
+          src={GetLucky}
+          onplay="play"
+          autoPlay={this.state.audioPlay}
+        ></audio>
       </div>
     );
   }
